0c99e1028d0d823d206aec8a3bd2514c8df2bfad
LAPTOP-7MKTAQ7K\USER <a0970797036@gmail.com>
2025-09-25 17:40:47 +0800

    fix: 靜態建置錯誤（動態路由改為非預先產生）\nchore: 設定 astro.config.mjs site 為 GitHub Pages 網址

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://xd1104.github.io/LoveReceipe',
   vite: {
     plugins: [tailwindcss()]
   }
